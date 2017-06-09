5029587b6ca8c63e337421c9ce9275954d78dbf1
Mark Littlehorn <markdlittlehorn@gmail.com>
2017-06-09 09:47:58 +0000

    Fixed environment syntax

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'aqueous-cliffs-32566.herokuapp.com'
+  :domain         => 'aqueous-cliffs-32566.herokuapp.com',
   :authentication => :plain,
 }
-Actionmailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :smtp
