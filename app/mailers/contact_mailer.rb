9d7b2264ba645d657e6140e9758645b997e991c7
Mark Littlehorn <markdlittlehorn@gmail.com>
2017-06-09 10:38:02 +0000

    Changed email route

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'teamdevmatch@gmail.com'
+  default to: 'markdlittlehorn@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
