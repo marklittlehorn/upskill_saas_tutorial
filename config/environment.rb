88a80de8f94f5f65bce587fd1164c61aeed25ed9
Mark Littlehorn <markdlittlehorn@gmail.com>
2017-06-09 10:53:22 +0000

    changed domain in environment

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'aqueous-cliffs-32566.herokuapp.com',
+  :domain         => 'https://aqueous-cliffs-32566.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
